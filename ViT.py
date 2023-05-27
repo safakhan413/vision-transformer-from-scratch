aa3e30823e5c1e578fc136b8f54365b8e3115092
Safa Nasir <safakhan413@gmail.com>
2023-05-27 13:44:44 -0400

    comments added regarding architecture

--- ViT.py
+++ ViT.py
@@ -178,3 +178,27 @@ if __name__ == '__main__':
 # So our model inputs are: images of pizza, steak and sushi.
 
 # And our ideal model outputs are: predicted labels of pizza, steak or sushi.
+# Figure 1, Table 3.1 containing Mathematical equations, Table 1 showing hyperparameter settings will be our guiding light to create the ViT architecture to solve our problem
+
+# Steps in Figure 1 Explained:
+# 
+# Patch + Position Embedding (inputs) - Turns the input image into a sequence of image patches and add a position number what order the patch comes in.
+
+# Linear projection of flattened patches (Embedded Patches) - The image patches get turned into an embedding, the benefit of using an embedding rather 
+#   than just the image values is that an embedding is a learnable representation (typically in the form of a vector) of the image that can improve with training.
+
+# Norm - This is short for "Layer Normalization" or "LayerNorm", a technique for regularizing (reducing overfitting) a neural network, you can use 
+#   LayerNorm via the PyTorch layer torch.nn.LayerNorm().
+
+# Multi-Head Attention - This is a Multi-Headed Self-Attention layer or "MSA" for short. You can create an MSA layer via the PyTorch layer torch.nn.MultiheadAttention().
+#   Image is broken down into patches rather than performing Self Attention on each of the layers so that computation cost
+
+# MLP (or Multilayer perceptron) - A MLP can often refer to any collection of feedforward layers (or in PyTorch's case, a collection of layers with a forward() method). 
+#   In the ViT Paper, the authors refer to the MLP as "MLP block" and it contains two torch.nn.Linear() layers with a torch.nn.GELU() non-linearity activation
+#   in between them (section 3.1) and a torch.nn.Dropout() layer after each (Appendex B.1).
+
+# Transformer Encoder - The Transformer Encoder, is a collection of the layers listed above. There are two skip connections inside the Transformer encoder
+#   (the "+" symbols) meaning the layer's inputs are fed directly to immediate layers as well as subsequent layers. The overall ViT architecture is
+#   comprised of a number of Transformer encoders stacked on top of eachother.
+
+# MLP Head - This is the output layer of the architecture, it converts the learned features of an input to a class output. Since we're working on image classification, you could also call this the "classifier head". The structure of the MLP Head is similar to the MLP block. 
